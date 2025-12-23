b55611411c6104a0e96733ce768917e6870b5c87
Amine Ben Abla <aminebenabla@MacBook-Pro-de-Amine-2.local>
2025-12-23 17:58:59 +0100

    fix: add dynamic to clients/new page

--- src/app/(dashboard)/clients/new/page.tsx
+++ src/app/(dashboard)/clients/new/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import { ClientEditor } from "@/components/features/ClientEditor";
 
+export const dynamic = 'force-dynamic';
+
 export default function NewClientPage() {
     return (
         <Suspense fallback={<div>Chargement...</div>}>
